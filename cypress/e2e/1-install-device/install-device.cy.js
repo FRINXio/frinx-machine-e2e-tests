56376bb1477268a3a8284d707e3c0c765209314d
Simon Misencik <simon.misencik@gmail.com>
2022-08-08 08:58:42 +0000

    Update wait time
    Signed-off-by: Simon Misencik <simon.misencik@gmail.com>

--- cypress/e2e/1-install-device/install-device.cy.js
+++ cypress/e2e/1-install-device/install-device.cy.js
@@ -17,10 +17,10 @@ describe('install IOS device', () => {
       cy.get('button').eq(25).click();
       cy.findByText('Close').click();
       cy.findByText('Executed').click();
-      cy.findAllByText('Install_device_by_name', { timeout: 10000000 }).should('be.visible');
+      cy.findAllByText('Install_device_by_name', { timeout: 10000 }).should('be.visible');
       cy.get('tbody').get('tr').eq(1).within(() => {
         cy.get('a').click();
       });
-      cy.findAllByText('COMPLETED', { timeout: 100000000 }).should('be.visible');
+      cy.findAllByText('COMPLETED', { timeout: 100000 }).should('be.visible');
     });
 })
