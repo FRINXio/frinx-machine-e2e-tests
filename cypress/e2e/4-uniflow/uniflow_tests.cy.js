fd5e2dcd100a9414baab645b22027d0d1f608910
Marcel Suleiman <marcelsuleiman@gmail.com>
2022-09-26 14:45:09 +0200

    skipped scheduled test

--- cypress/e2e/4-uniflow/uniflow_tests.cy.js
+++ cypress/e2e/4-uniflow/uniflow_tests.cy.js
@@ -6,26 +6,37 @@ describe('master test for checking UniFlow features', () => {
 	})
 
 	it('workflow page', () => {
-		cy.get('button[class="chakra-button chakra-menu__menu-button css-1unm9xp"]').click()
-		cy.get('p[class="chakra-text css-722v25"]').eq(1).click()
+		//Check UniFlow access by menulist
+		cy.xpath('/html/body/div[1]/div[1]/div[1]/button').click()
+		cy.get("a[data-index='1'").click()
+		// test
 		cy.get('h1').should('include.text', 'Workflows')
+
+		//Check UniFlow access by dashboard link
 		cy.visit(Cypress.env('host'))
 		cy.contains('a', 'UniFlow').click()
+		// test
 		cy.get('h1').should('include.text', 'Workflows')
 	})
 	
 	it('filter by label', () => {
 		cy.get('[href="/frinxui/uniflow/definitions"]').click()
 		cy.url().should('include', 'definitions')
+
 		cy.get('input[placeholder="Search by label."').type('CLI{enter}')
 		cy.wait(300)
+
+		// test
 		cy.get('tbody').find("tr")
 			.then((row) => {
 				expect(row.length).eq(6)
 			})
 
+		// delete
 		cy.contains('p', 'CLI').click()
+
 		cy.get('input[placeholder="Search by label."').type('BASICS{enter}')
+		// test
 		cy.get('tbody').find("tr")
 			.then((row) => {
 				expect(row.length).eq(10)
@@ -38,13 +49,20 @@ describe('master test for checking UniFlow features', () => {
 				expect(row.length).eq(3)
 			})
 
+		// delete
 		cy.contains('p', 'BASICS').click()
+
+		// set 2 unlogical labels
+		// manually doesn't work properly - it's hard to reproduce by physically typing
 		cy.get('input[placeholder="Search by label."').type('CLI{enter}').type('L2VPN{enter}')
 		cy.wait(300)
+		// test
 		cy.get('tbody tr').should(($tr) => {
 			expect($tr).to.have.length(0)
 		})
-		cy.get('.chakra-input__right-element > .chakra-button').click()
+
+		cy.get('button[aria-label="Clear"]').click()
+		// pseudo test
 		cy.contains('button', '3').click()
 	})
 
@@ -70,9 +88,10 @@ describe('master test for checking UniFlow features', () => {
 				expect(row.length).eq(10)
 			})
 	})
+
 	it('wf uninstall all devices', () => {
-		cy.get('button[class="chakra-button chakra-menu__menu-button css-1unm9xp"]').click()
-		cy.get('p[class="chakra-text css-722v25"]').eq(1).click()
+		cy.xpath('/html/body/div[1]/div[1]/div[1]/button').click()
+		cy.get("a[data-index='1'").click()
 		cy.url().should('include', 'definitions')
 		cy.get('input[placeholder="Search by keyword."').type('Uninstall_all_from_inventory')
 		cy.wait(300)
@@ -84,16 +103,20 @@ describe('master test for checking UniFlow features', () => {
 		cy.get('input[placeholder="Enter the input"]').type('CREATE_LOOPBACK_DEMO')
 		cy.contains('button', 'Execute').click()
 		cy.contains('a', '-', {timeout: 10000}).click()
+
+		// wf_id will be string usable to back-end api test
 		cy.url().then(urlString => {
 			let wf_id_array = urlString.split('/')
 			const wf_id = wf_id_array[wf_id_array.length - 1]
 		})
+
+		//test
 		cy.contains('div', 'Status', {timeout: 1200000}).should('contain', 'COMPLETED')
 	})
 	
 	it('wf install all devices', () => {
-		cy.get('button[class="chakra-button chakra-menu__menu-button css-1unm9xp"]').click()
-		cy.get('p[class="chakra-text css-722v25"]').eq(1).click()
+		cy.xpath('/html/body/div[1]/div[1]/div[1]/button').click()
+		cy.get("a[data-index='1'").click()
 		cy.url().should('include', 'definitions')
 		cy.get('input[placeholder="Search by keyword."').type('Install_all_from_inventory')
 		cy.wait(300)
@@ -105,35 +128,40 @@ describe('master test for checking UniFlow features', () => {
 		cy.get('input[placeholder="Enter the input"]').type('CREATE_LOOPBACK_DEMO')
 		cy.contains('button', 'Execute').click()
 		cy.contains('a', '-', {timeout: 10000}).click()
+		
+		// wf_id will be string usable to back-end api test
 		cy.url().then(urlString => {
 			let wf_id_array = urlString.split('/')
 			const wf_id = wf_id_array[wf_id_array.length - 1]
 		})
-		cy.xpath('/html/body/div[1]/div[2]/div/div[2]/div/div[4]/div/text()').then(status =>{
-			cy.log(status)
-		})
+
+		//test
 		cy.contains('div', 'Status', {timeout: 1200000}).should('contain', 'COMPLETED')
 	})
 	
 	it('try add wf to favourite', () => {
 		cy.get('[href="/frinxui/uniflow/definitions"]').click()
-		cy.get("button[class='chakra-button chakra-menu__menu-button css-byrfz9']").eq(0).click()
-		cy.get("button[class='chakra-menu__menuitem css-13c7rae']").eq(0).click()
+		cy.wait(500)
+		cy.get('button[aria-haspopup="menu"]').eq(2).click()
+		cy.contains('button', 'Add to favourites').click()
 		cy.get('button[title="Favourites"]').click()
 		cy.wait(500)
+		// test
 		cy.get('tbody').find("tr")
 			.then((row) => {
 				expect(row.length).eq(1)
 			})
-		cy.get("button[class='chakra-button chakra-menu__menu-button css-byrfz9']").eq(0).click()
-		cy.get("button[class='chakra-menu__menuitem css-13c7rae']").eq(0).click()
+		cy.get('button[aria-haspopup="menu"]').eq(2).click()
+		cy.contains('button', 'Remove from favourites').click()
 	})
 
 	it.skip('try add wf to scheduled', () => {
 		cy.get('[href="/frinxui/uniflow/definitions"]').click()
 		cy.get('input[placeholder="Search by keyword."').type('Create_loopback_')
-		cy.get("button[class='chakra-button chakra-menu__menu-button css-byrfz9']").eq(0).click()
-		cy.get("button[class='chakra-menu__menuitem css-13c7rae']").wait(500).eq(3).click()
+		cy.get('button[aria-haspopup="menu"]').eq(2).click()
+		cy.contains('button', 'Create schedule').click()
+
+		// generate cron tab value -> +2 min from now
 		var now = new Date(); 
 		var year = now.getFullYear();
 		var month = now.getMonth()+1; 
@@ -142,29 +170,38 @@ describe('master test for checking UniFlow features', () => {
 		var minute = now.getMinutes()+1;
 		var second = now.getSeconds();
 		var crontab_value = minute + ' ' + hour + ' ' + day + ' ' + month + ' ' + '*'
+
 		cy.xpath('/html/body/div[4]/div[4]/div/section/div/form/div[1]/input').clear().type(crontab_value)
 		cy.xpath('/html/body/div[4]/div[4]/div/section/div/form/div[2]/label/span[1]').click()
+
 		var wf_content = `{"input":{"labels":"CREATE_LOOPBACK_DEMO"},"scheduleName":"frinx___Create_loopback_all_in_uniconfig:1","labels":"CREATE_LOOPBACK_DEMO","loopback_id":"789"}}`
+		
 		cy.get('.ace_content').type('{backspace}{backspace}')
 		cy.get('.ace_content').type(wf_content, { parseSpecialCharSequences: false })
 		cy.xpath('/html/body/div[4]/div[4]/div/section/footer/div/button[1]').click()
 		cy.get('[href="/frinxui/uniflow/scheduled"]').click()
 		cy.wait(300)
+
+		// test
 		cy.get('tbody', {timeout:2500}).find("tr")
 			.then((row) => {
 				expect(row.length).eq(1)
 			})
 		cy.xpath('/html/body/div[1]/div[2]/div/table/tbody/tr/td[5]/div/div/button[2]').click()
+
+		//test
 		cy.xpath('/html/body/div[4]/div[4]/div/section/div/form/div[1]/input')
 			.invoke('val')
 			.then((crontab_value_2) => {
 				cy.log(crontab_value_2);
 				expect(crontab_value_2).eq(crontab_value)
 			})
+		
+
+		// test
 		cy.get('.chakra-checkbox__control').should('have.attr', 'data-checked') 
-		cy.xpath('/html/body/div[5]/div[4]/div/section/div/form/div[2]/label/input')
-			.check({ force: true })
-			.should('be.checked')
+
+		//test
 		cy.xpath('/html/body/div[4]/div[4]/div/section/div/form/div[3]/div/div[2]/div/div[3]/div[6]/div/span[2]')
 			.invoke('text')
 			.then((loopback_id) => {
@@ -226,8 +263,8 @@ describe('master test for checking UniFlow features', () => {
 	})
 
 	it('try create wf', () => {
-		cy.get('button[class="chakra-button chakra-menu__menu-button css-1unm9xp"]').click()
-		cy.get('p[class="chakra-text css-722v25"]').eq(1).click()
+		cy.xpath('/html/body/div[1]/div[1]/div[1]/button').click()
+		cy.get("a[data-index='1'").click()
 		cy.url().should('include', 'definitions')
 		cy.xpath('/html/body/div[1]/div[2]/div[1]/header/div/div/a').click()
 		cy.xpath('/html/body/div[1]/div[2]/div/div/div/form/div[1]/input').type('test_name_001')
@@ -242,6 +279,7 @@ describe('master test for checking UniFlow features', () => {
 		cy.url().should('include', 'builder')
 		cy.get('[href="/frinxui/uniflow/definitions"]').click()
 		cy.get('input[placeholder="Search by keyword."').type('test_name_001')
+		// test
 		cy.get('tbody tr').should(($tr) => {
 			expect($tr).to.have.length(0)
 		})
