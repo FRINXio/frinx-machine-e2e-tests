e2b69b63ebe4c636f7443e125b9c0702a7b875b3
Šimon Mišenčík <simon.misencik@gmail.com>
2022-12-02 13:49:57 +0100

    Update workflow_manager_tests.cy.js

--- cypress/e2e/4-workflow-manager/workflow_manager_tests.cy.js
+++ cypress/e2e/4-workflow-manager/workflow_manager_tests.cy.js
@@ -18,7 +18,7 @@ describe('master test for checking workflow manager features', () => {
     cy.get('h1').should('include.text', 'Workflows')
   })
 
-  it.skip('filter by label', () => {
+  it('filter by label', () => {
     cy.get('[href=\'/frinxui/workflow-manager/definitions\']').click()
     cy.url().should('include', 'definitions')
 
@@ -150,7 +150,7 @@ describe('master test for checking workflow manager features', () => {
     cy.contains('div', 'Status', { timeout: 300000 }).should('contain', 'COMPLETED')
   })
 
-  it.skip('try add wf to scheduled', () => {
+  it('try add wf to scheduled', () => {
     cy.get('[href=\'/frinxui/workflow-manager/definitions\']').click()
     cy.get('input[placeholder=\'Search by keyword.\'').type('Create_loopback_')
     cy.get('button[aria-haspopup=\'menu\']').eq(2).click()
